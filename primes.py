2f945e46f3c393ca59b3de27dd369035df0004d7
Oli Iliffe <oliver.iliffe@kcl.ac.uk>
2022-10-04 22:22:37 +0100

    complete task

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+def primes(number_of_primes: int) -> list[int]:
+    """
+    generates the first `number_of_primes` primes and returns them as 
+    a list
+    """
+    if number_of_primes < 1:
+        raise ValueError(
+            f"number_of_primes must be 1 or more, got {number_of_primes}")
 
-def primes(number_of_primes):
-    list = []
-    return list
+    primes = [2]
+    for _ in range(number_of_primes - 1):
+        tmp = primes[-1]
+        while True:
+            tmp += 1
+            for prime in primes:
+                if tmp % prime == 0:
+                    break
+            else:
+                primes.append(tmp)
+                break
+            
+    return primes
